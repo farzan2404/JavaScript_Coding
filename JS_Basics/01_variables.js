6b51c4bbf5045e233fbafda488d6d93ca38be04d
Mohammad Farzan Farooqui <85079257+farzan2404@users.noreply.github.com>
2024-06-01 14:31:19 +0000

    configuration commit done

---
+++ JS_Basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = "1"
+let accName = "Farzan"
+var rating = 4.9/5.0
+standards = "Perfect"
+
+console.table([accountId,accName,rating,standards])
---
+++ JS_Basics/03_ConversionOperation.js
@@ -0,0 +1,13 @@
+let string = "33ab"
+let string2 = Number(string)
+
+console.log(string2) // 33ab not a number
+console.log(typeof string2)
+
+
+
+// Boolean checking.
+let isBoolean = ""
+let finalBoolean = Boolean(isBoolean)
+
+console.log(finalBoolean)
