a075dbe80fc2bfaf728a3cb1dc24e814365d943f
Mohammad Farzan Farooqui <85079257+farzan2404@users.noreply.github.com>
2024-06-01 18:38:26 +0000

    Changes Done

--- JS_Basics/03_ConversionOperation.js
+++ JS_Basics/03_ConversionOperation.js
@@ -1,13 +1,19 @@
 let string = "33ab"
 let string2 = Number(string)
 
-console.log(string2) // 33ab not a number
-console.log(typeof string2)
-
+// console.log(string2) // 33ab not a number
+// console.log(typeof string2)
 
 
 // Boolean checking.
 let isBoolean = ""
 let finalBoolean = Boolean(isBoolean)
 
-console.log(finalBoolean)
+// console.log(finalBoolean)
+
+// *********************Operations*****************
+
+console.log(2 + 2 + 3);
+console.log("2" + 2);
+console.log(2 + 3 + "3");
+console.log(("2"+ 2 + 1));
---
+++ JS_Basics/04_Comparision.js
@@ -0,0 +1,8 @@
+// console.log("2" > 1);
+// console.log(1 == "2");
+console.log(2 == "2") // return true, because it simply converts to make similar
+console.log(2 === "2") // return false, because it will check the data type.
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // Here null will be converted to 0, that's why it's equal and true.
